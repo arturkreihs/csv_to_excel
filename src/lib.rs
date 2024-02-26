851de12fbe352c6e046f71a5fa9543ab53c0266c
Artur Kreihs <akreihs@ascs.pl>
2024-02-26 16:23:12 +0100

    . parse first line as headers

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,15 @@ pub fn convert(csv: &str) -> Result<Vec<u8>, XlsxError> {
     let mut workbook = Workbook::new();
     let worksheet = workbook.add_worksheet();
     let mut rdr = Reader::from_reader(csv.as_bytes());
+    let headers = rdr.headers()
+        .map_err(|_| XlsxError::CustomError("headers can't be obtained".into()))?;
+    for (col_index, cell) in headers.iter().enumerate() {
+        worksheet.write_string(0, col_index as u16, cell)?;
+    }
     for (row_index, row) in rdr.records().enumerate() {
         if let Ok(record) = row {
             for (col_index, cell) in record.iter().enumerate() {
-                worksheet.write_string(row_index as u32, col_index as u16, cell)?;
+                worksheet.write_string((row_index + 1) as u32, col_index as u16, cell)?;
             }
         }
     }
