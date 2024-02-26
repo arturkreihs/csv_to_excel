fd53f55513c19f30b397185504039faf1864eb9b
Artur Kreihs <akreihs@ascs.pl>
2024-02-26 16:45:21 +0100

    . header formatting;

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,18 @@
 use csv::Reader;
-use rust_xlsxwriter::Workbook;
+use rust_xlsxwriter::{ Workbook, Format, Color };
 pub use rust_xlsxwriter::XlsxError;
 
 pub fn convert(csv: &str) -> Result<Vec<u8>, XlsxError> {
     let mut workbook = Workbook::new();
     let worksheet = workbook.add_worksheet();
     let mut rdr = Reader::from_reader(csv.as_bytes());
+    let headers_format = Format::new()
+        .set_bold()
+        .set_background_color(Color::Silver);
     let headers = rdr.headers()
         .map_err(|_| XlsxError::CustomError("headers can't be obtained".into()))?;
     for (col_index, cell) in headers.iter().enumerate() {
-        worksheet.write_string(0, col_index as u16, cell)?;
+        worksheet.write_string_with_format(0, col_index as u16, cell, &headers_format)?;
     }
     for (row_index, row) in rdr.records().enumerate() {
         if let Ok(record) = row {
